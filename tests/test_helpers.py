5c411469c9d8c7f4a6e83d23f4f08751970b99ad
Daniel Roy Greenfeld <62857+pydanny@users.noreply.github.com>
2024-03-24 17:46:20 +0000

    Posts with zero tags no longer generate errors

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -3,8 +3,8 @@ from fastapi_blog import helpers
 
 def test_list_published_posts_success():
     posts = helpers.list_posts(posts_dirname="tests/examples/posts")
-    assert len(posts) == 18
-    assert posts[0]["title"] == "Three Years at Kraken Tech"
+    assert len(posts) == 19
+    assert posts[0]["title"] == "No Tags"
     assert posts[-1]["title"] == "Code, Code, Code"
 
 
